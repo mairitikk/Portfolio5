7f8761f063bedf9c116b85b5ac1bf4249d054b8e
mairi tikk <mairi.tikk@gmail.com>
2024-09-26 12:37:02 +0200

    end reducer setup

--- redux-ts/src/state/reducers/repositoriesReducer.ts
+++ redux-ts/src/state/reducers/repositoriesReducer.ts
@@ -6,8 +6,11 @@ interface RepositoriesState {
 const reducer = (state: RepositoriesState, action: any) => {
   switch (action.type) {
     case "search_repositories":
+      return { loading: true, error: null, data: [] };
     case "search_repositories_success":
+      return { loading: false, error: null, data: action.payload };
     case "search_repositories_error":
+      return { loading: false, error: action.payload, data: [] };
     default:
       return state;
   }
