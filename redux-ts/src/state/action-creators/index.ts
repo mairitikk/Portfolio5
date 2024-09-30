a6868089f0e269bcb552a4b0593c25773291a43d
mairi tikk <mairi.tikk@gmail.com>
2024-09-30 12:28:37 +0200

    add try catch

--- redux-ts/src/state/action-creators/index.ts
+++ redux-ts/src/state/action-creators/index.ts
@@ -3,5 +3,16 @@ import { ActionType } from "../action-types";
 import { Action } from "../actions";
 
 const searchRepositories = (term: string) => {
-  return async (dispatch: any) => {};
+  return async (dispatch: any) => {
+    dispatch({
+      type: ActionType.SEARCH_REPOSITORIES,
+    });
+    try {
+    } catch (err: any) {
+      dispatch({
+        type: ActionType.SEARCH_REPOSITORIES_ERROR,
+        payload: err.message,
+      });
+    }
+  };
 };
