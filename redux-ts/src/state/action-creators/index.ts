32c6dc7cb4cb1dc3ba7cec40de2eacd113f3ad89
mairi tikk <mairi.tikk@gmail.com>
2024-09-30 12:24:49 +0200

    add imports

---
+++ redux-ts/src/state/action-creators/index.ts
@@ -0,0 +1,7 @@
+import axios from "axios";
+import { ActionType } from "../action-types";
+import { Action } from "../actions";
+
+const searchRepositories = (term: string) => {
+  return async (dispatch: any) => {};
+};
