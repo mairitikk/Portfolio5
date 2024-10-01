0be119bed554a1f64ef63227fb73d951066da14f
mairi tikk <mairi.tikk@gmail.com>
2024-10-01 10:20:15 +0200

    add Dispatch redux

--- redux-ts/src/state/action-creators/index.ts
+++ redux-ts/src/state/action-creators/index.ts
@@ -1,9 +1,10 @@
 import axios from "axios";
+import { Dispatch } from "redux";
 import { ActionType } from "../action-types";
 import { Action } from "../actions";
 
-const searchRepositories = (term: string) => {
-  return async (dispatch: any) => {
+export const searchRepositories = (term: string) => {
+  return async (dispatch: Dispatch<Action>) => {
     dispatch({
       type: ActionType.SEARCH_REPOSITORIES,
     });
@@ -18,6 +19,11 @@ const searchRepositories = (term: string) => {
       );
       const names = data.objects.map((result: any) => {
         return result.package.name;
+
+        dispatch({
+          type: ActionType.SEARCH_REPOSITORIES_SUCCESS,
+          payload: names,
+        });
       });
     } catch (err: any) {
       dispatch({
