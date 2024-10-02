dee7584eea588f89ea7856b5acaccca25ca83bf1
mairi tikk <mairi.tikk@gmail.com>
2024-10-02 10:15:23 +0200

    add RepositoriesList

--- redux-ts/src/components/App.tsx
+++ redux-ts/src/components/App.tsx
@@ -1,7 +1,14 @@
 
 import { Provider } from 'react-redux';
 import { store } from '../state';
+import RepositoriesList from './RepositoriesList';
 const App = () => {
-    return <h1>Tere</h1>
+    return <Provider store={store}>
+        <div className="App">
+            <h1>Search For a Package</h1>
+            <RepositoriesList></RepositoriesList>
+        </div>
+    </Provider>
+   
 };
 export default App;
---
+++ redux-ts/src/components/RepositoriesList.tsx
@@ -0,0 +1,10 @@
+const RepositoriesList: React.FC = () => {
+    return (<div>
+        <form action="">
+            <input type="text" name="search" placeholder="Search" />
+            <button>Search</button>
+        </form>
+    </div>
+    );
+};
+export default RepositoriesList;
