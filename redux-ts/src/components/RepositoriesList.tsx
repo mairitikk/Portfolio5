27279b169aff2f8dcaf7d6cf988172cbee5f7745
mairi tikk <mairi.tikk@gmail.com>
2024-10-02 11:37:23 +0200

    add useState

--- redux-ts/src/components/RepositoriesList.tsx
+++ redux-ts/src/components/RepositoriesList.tsx
@@ -1,4 +1,8 @@
+import { useState } from 'react';
+
+
 const RepositoriesList: React.FC = () => {
+    const [term, setterm] = useState('');
     return (<div>
         <form action="">
             <input type="text" name="search" placeholder="Search" />
