e90cf66e8565f03b79616c0e70c40d3e6903cb73
mairi tikk <mairi.tikk@gmail.com>
2024-10-02 11:46:32 +0200

    add onSubmit and onChange

--- redux-ts/src/components/RepositoriesList.tsx
+++ redux-ts/src/components/RepositoriesList.tsx
@@ -3,9 +3,13 @@ import { useState } from 'react';
 
 const RepositoriesList: React.FC = () => {
     const [term, setterm] = useState('');
+
+    const onSubmit = () => {
+
+    };
     return (<div>
-        <form action="">
-            <input type="text" name="search" placeholder="Search" />
+        <form onSubmit={onSubmit} action="">
+            <input value={term} onChange={e => setterm(e.target.value)} type="text" name="search" placeholder="Search" />
             <button>Search</button>
         </form>
     </div>
