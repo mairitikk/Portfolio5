04ab547d4ff564036770835a09eda349255bf42b
mairi tikk <mairi.tikk@gmail.com>
2024-10-04 09:46:58 +0200

    delete  useDispatch

--- redux-ts/src/hooks/useActions.ts
+++ redux-ts/src/hooks/useActions.ts
@@ -1,2 +1,8 @@
 import { useDispatch } from "react-redux";
 import { bindActionCreators } from "redux";
+import { actionCreators } from "../state";
+
+export const useActions = () => {
+  const dispatch = useDispatch();
+  bindActionCreators(actionCreators, dispatch);
+};
