df34f4a91be18bdce442678f49a96a4522b74257
mairi tikk <mairi.tikk@gmail.com>
2024-10-04 10:12:37 +0200

    export reducers

--- redux-ts/src/components/RepositoriesList.tsx
+++ redux-ts/src/components/RepositoriesList.tsx
@@ -1,25 +1,28 @@
 import { useState } from 'react';
-import { useSelector } from 'react-redux';
+import { useTypedSelector } from '../hooks/useTypedSelector';
 import { useActions } from '../hooks/useActions';
 
-
-
 const RepositoriesList: React.FC = () => {
-    const [term, setterm] = useState('');
-    const {searchRepositories }:any  = useActions();
+  const [term, setTerm] = useState('');
+  const { searchRepositories } = useActions();
+  const { data, error, loading } = useTypedSelector(
+    (state) => state.repositories
+  );
 
-    const onSubmit = (event:  React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
 
-        searchRepositories(term) as any;
+    searchRepositories(term);
+  };
 
-    };
-    return (<div>
-        <form onSubmit={onSubmit} action="">
-            <input value={term} onChange={e => setterm(e.target.value)} type="text" name="search" placeholder="Search" />
-            <button>Search</button>
-        </form>
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input value={term} onChange={(e) => setTerm(e.target.value)} />
+        <button>Search</button>
+      </form>
     </div>
-    );
+  );
 };
+
 export default RepositoriesList;
