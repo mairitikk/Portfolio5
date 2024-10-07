9d6f031b06bc05b150c3e01974eaf7c62b8325fa
mairi tikk <mairi.tikk@gmail.com>
2024-10-07 09:53:13 +0200

    add repositoriesList

--- redux-ts/src/components/RepositoriesList.tsx
+++ redux-ts/src/components/RepositoriesList.tsx
@@ -1,11 +1,10 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { useState } from 'react';
 import { useTypedSelector } from '../hooks/useTypedSelector';
 import { useActions } from '../hooks/useActions';
 
 const RepositoriesList: React.FC = () => {
   const [term, setTerm] = useState('');
-    const { searchRepositories } = useActions();
+  const { searchRepositories } = useActions();
   const { data, error, loading } = useTypedSelector(
     (state) => state.repositories
   );
@@ -22,8 +21,12 @@ const RepositoriesList: React.FC = () => {
         <input value={term} onChange={(e) => setTerm(e.target.value)} />
         <button>Search</button>
       </form>
+      {error && <h3>{error}</h3>}
+      {loading && <h3>Loading...</h3>}
+      {!error && !loading && data.map((name) => <div key={name}>{name}</div>)}
     </div>
   );
 };
 
 export default RepositoriesList;
+
